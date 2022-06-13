f7e08c3a596b555b792b7e04822b383c68fbdffb
Alvaro Pereira do Nascimento <alvaro.nascimentoapn@gmail.com>
2022-06-13 16:58:32 -0300

    Inserido classe para padronização de chamada de diálogo customizado.

---
+++ DialogMessage/Dialogs/CustomDialog.cs
@@ -0,0 +1,18 @@
+﻿using DialogMessage.Dialogs.Model;
+using DialogMessage.Dialogs.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogMessage.Dialogs
+{
+    public class CustomDialog
+    {
+        public static bool? ShowMessage(DialogModel dialogModel)
+        {
+            return new Dialog(dialogModel).ShowDialog();
+        }
+    }
+}
