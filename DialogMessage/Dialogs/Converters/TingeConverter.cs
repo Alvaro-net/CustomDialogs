e9616601ff1d22eaef81016df8f0d135437d4936
Alvaro Pereira do Nascimento <alvaro.nascimentoapn@gmail.com>
2022-06-13 16:55:47 -0300

    Inserido conversor de string Hex de color para uma cor hexa

---
+++ DialogMessage/Dialogs/Converters/TingeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace DialogMessage.Dialogs.Converters
+{
+    public static class TingeConverter
+    {
+        public static SolidColorBrush ToBrush(this string hexColorString)
+        {
+            return (SolidColorBrush)(new BrushConverter().ConvertFrom(hexColorString));
+        }
+
+        public static Color ToColor(this string hexColorString)
+        {
+            return (Color)ColorConverter.ConvertFromString(hexColorString);
+        }
+    }
+}
